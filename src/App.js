4c4e57de26be874eb259918fec8670d6eafcc615
Rohan-Sterling <rohansterling@pursuit.org>
2021-11-14 14:22:37 -0500

    add handleUserInput and handleCalc methods in App.js, add handleUserInput and handleOperClick methods the Button.js

--- src/App.js
+++ src/App.js
@@ -7,19 +7,39 @@ class App extends Component {
     super()
     this.state = {
      userInput: '',
+     result: '',
     }
   }
   
+  handleUserInput = (value, isEqual) => {
+    if(isEqual){
+      this.setState({userInput: this.state.result})
+    } else {
+      this.setState({userInput: !value.length ? 0 : this.state.userInput + value})
+    }
+    if(this.state.userInput === 0){
+      this.setState({userInput: value})
+    }
+  }
 
-
-
+  handleCalc = (expression) => {
+    if ((/\d/).test(expression[expression.length-1])){
+      this.setState({result: eval(expression)})
+    }
+    if(!expression.length){
+      this.setState({result: expression})
+    }
+  }
+  
+  
   render() {
       return (
         <div className="App">
           <div className="display">
-            <div>{this.state.userInput || 0}</div>
+            <div className="userInput">{this.state.userInput || 0}</div>
+            <div className="result">{this.state.result}</div>
             </div>
-          <Buttons/>
+          <Buttons handleCalc={this.handleCalc} userInput={this.handleUserInput}/>
       </div>
     );
   }
--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -3,6 +3,50 @@ import './Buttons.css'
 
 
 export default class Buttons extends Component {
+    constructor(){
+        super()
+        this.state = {
+            expression: ''
+        }
+    }
+
+    handleUserInput = (event) => {
+        const exStr = this.props.handleCalc
+        const eValue = event.target.value
+        const { expression } = this.state
+        this.setState({expression: expression + eValue})
+        this.props.userInput(eValue)
+        exStr(expression + eValue)
+    }
+
+    handleOperClick = (event) => {
+        const userInput = this.props.userInput
+        const exStr = this.props.handleCalc
+        const eValue = event.target.value
+        const { expression } = this.state
+        if((/\+|-|÷|×/).test(eValue)){
+            let newValue = eValue
+            if(eValue === '÷'){
+                newValue = '/'
+            } else if (eValue === '×'){
+                newValue = '*'
+            }
+            this.setState({expression: expression + newValue})
+
+            userInput(eValue)
+            exStr(expression)
+        }
+        if(eValue === '='){
+            userInput('', true)
+            exStr(expression)
+        }
+        if(eValue === 'AC'){
+            this.setState({expression: ''})
+            userInput('', false)
+            exStr('')
+        }
+        
+      }
 
      buildNumPad = () => {
         const numbers = [7,8,9,4,5,6,1,2,3,0,'.']
@@ -10,17 +54,17 @@ export default class Buttons extends Component {
      }
 
      buildOperationsPad = () => {
-         const operations = "AC,+/-,+,-,x,=,/"
+         const operations = "AC,±,+,-,×,=,÷"
          return operations.split(',').map((element, i) => <button className={'oper-'+ i} key={element} value={element}>{element}</button>)
      }
      
     render(){
         return (
             <>
-            <section className="numpad">
+            <section onClick={this.handleUserInput} className="numpad">
                 {this.buildNumPad()}
             </section>
-            <section className="operations-pad">
+            <section onClick={this.handleOperClick} className="operations-pad">
                 {this.buildOperationsPad()}      
             </section>
             </>
